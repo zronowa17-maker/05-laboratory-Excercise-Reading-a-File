81e4ca3528ee96abf177cd50258d3cec8ad13ece
zronowa17-maker <zronowa17@gmail.com>
2025-10-23 19:34:04 +0800

    design more

--- Reading a File/FrmStudentRecord.Designer.cs
+++ Reading a File/FrmStudentRecord.Designer.cs
@@ -39,6 +39,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(20, 27);
             this.label1.Name = "label1";
